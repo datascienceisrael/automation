65738ba12f075d77d32bb535f7bf69bd5ec9d810
Or Chen <50183706+spinelsun@users.noreply.github.com>
2019-07-25 14:12:06 +0300

    add conftest.py module
    a module that contains any pytest fixture required by more than one test module

---
+++ project_generator/{{ cookiecutter.repo_name }}/tests/conftest.py
@@ -0,0 +1,7 @@
+"""A module that contains all project-global pytest fixtures.
+There is no need to import fixtures from this module; they are automatically
+get discovered by pytest.
+For more information about fixtures click the following link:
+http://doc.pytest.org/en/latest/fixture.html
+"""
+import pytest
