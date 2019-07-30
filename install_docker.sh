6a22027c14c4cb53f2c75f283887f87fd6bc59e4
Pavel Suchman <pavels@datascience.co.il>
2019-07-30 17:42:52 +0300

    Docker installation for Ubuntu - initial commit

---
+++ install_docker.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Install and configure Docker-ce on Ubuntu
+
+docker_apt="/etc/apt/sources.list.d/docker.list"
+
+if ! [[ -f $docker_apt ]]
+    then
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    sudo sh -c "echo deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable> $docker_apt"
+fi
+
+if ! dpkg -l | grep docker-ce > /dev/null
+    then
+    sudo apt-get update
+    sudo apt-get install -y apt-transport-https
+    sudo apt-get install -y software-properties-common
+    sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+fi
+
+# Give the current user rw permissions on docker socket
+sudo setfacl -m user:$USER:rw /var/run/docker.sock
