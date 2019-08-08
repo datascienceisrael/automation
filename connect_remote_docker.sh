73f689b9a0959c356cd2f9d53fe5491316230bba
Pavel Suchman <pavels@datascience.co.il>
2019-08-08 15:55:10 +0300

    Use autossh instead of regular ssh tunnel

--- connect_remote_docker.sh
+++ connect_remote_docker.sh
@@ -2,5 +2,11 @@
 
 ssh_host=$1
 
+# Install autossh if needed
+if ! dpkg -l | grep autossh > /dev/null
+    then
+    sudo apt-get install -y autossh
+fi
+
 # Create tunnel
-ssh -NL localhost:23750:/var/run/docker.sock $ssh_host &
+autossh -M 0 -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -NL localhost:23750:/var/run/docker.sock $ssh_host &
